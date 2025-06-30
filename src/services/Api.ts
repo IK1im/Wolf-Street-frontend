2f91667c5ccc001ca0b32a8c5e8637ef38370d3c
IK1im <igklim04@gmail.com>
2025-06-30 17:59:58 +0300

    changes axios

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,26 +1,11 @@
 import axios from "axios";
 
-// Создаём экземпляр axios с базовыми настройками
 const api = axios.create({
-  baseURL: "https://your-api-url.com/api", // Замените на ваш базовый URL
-  timeout: 10000, // Таймаут запроса (10 секунд)
+  baseURL: "http://89.169.183.192:8080",
+  timeout: 15000,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-// Пример: перехватчик для добавления токена авторизации (если потребуется)
-/*
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => Promise.reject(error)
-)
-*/
 
 export default api;
